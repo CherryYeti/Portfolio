682dfb3a7563b97e899c24cd202551658172b259
Jason Javandel <jasonjavandel@gmail.com>
2022-07-16 11:44:36 -0700

    Fixed borked file

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,21 @@
-import Layout from '../components/layouts/main'
+import { AnimatePresence } from "framer-motion";
+import { ChakraProvider } from "@chakra-ui/react";
 import Fonts from '../components/fonts'
-import { AnimatePresence } from 'framer-motion'
-import Chakra from '../components/chakra'
+import Layout from '../components/layouts/main'
+import theme from '../lib/theme'
 
-if (typeof window !== 'undefined') {
-  window.history.scrollRestoration = 'manual'
+
+const Website = ({ Component, pageProps, router }) => {
+    return (
+        <ChakraProvider theme={theme}>
+            <Fonts />
+            <Layout router={router}>
+                <AnimatePresence exitBeforeEnter initial={true}>
+                    <Component {...pageProps} key={router.route} />
+                </AnimatePresence>
+            </Layout>
+
+        </ChakraProvider>
+    )
 }
-
-function Website({ Component, pageProps, router }) {
-  return (
-    <Chakra cookies={pageProps.cookies}>
-      <Fonts />
-      <Layout router={router}>
-        <AnimatePresence
-          exitBeforeEnter
-          initial={true}
-          onExitComplete={() => {
-            if (typeof window !== 'undefined') {
-              window.scrollTo({ top: 0 })
-            }
-          }}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-    </Chakra>
-  )
-}
-
 export default Website
