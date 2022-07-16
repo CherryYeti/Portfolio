3319e8bbc5fd6513ca3ef1d2298b4789bf10bab6
Jason Javandel <jasonjavandel@gmail.com>
2022-07-16 11:41:12 -0700

    t

--- pages/works/firestarter.js
+++ pages/works/firestarter.js
@@ -1,9 +1,8 @@
 import { Container, Badge, Link, List, ListItem, Button, SimpleGrid, GridItem } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Title, WorkImage, Meta } from "../../components/work";
+import { Title, Meta } from "../../components/work";
 import Footer from "../../components/footer";
 import Layout from "../../components/layouts/article";
-import Links from "../../components/links";
 import P from '../../components/paragraph'
 
 const Work = () => {
